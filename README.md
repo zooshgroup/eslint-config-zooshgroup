ce3b31630e8ffcaf8f9853c3b83e65b676ee0bcd
Barnabas Gema <barnabas.gema@zooshgroup.com>
2018-01-09 14:08:07 +0100

    removes react related rules

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # eslint-config-zooshgroup
-The eslint config primarily used at Zoosh Group
+The eslint config primarily used at Zoosh Group for plain JavaScript (i.e. non-react) projects.
+
+For React projects check out our other config: [`eslint-config-zooshgroup-react`](https://github.com/zooshgroup/eslint-config-zooshgroup-react)
+
+## Installation
+
+```
+npm i eslint-config-zooshgroup --save-dev
+```
--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 module.exports = {
-  "extends": "airbnb",
+  "extends": "airbnb-base",
   "env": {
-    "browser": true,
     "node": true,
-    "jest": true,
     "es6": true
   },
   "plugins": [
-    "redux-saga",
-    "react",
-    "jsx-a11y",
     "zooshgroup"
   ],
   "parserOptions": {
-    "ecmaVersion": 6,
+    "ecmaVersion": 7,
     "sourceType": "module",
-    "ecmaFeatures": {
-      "jsx": true
-    }
   },
   "rules": {
     "arrow-parens": [
@@ -47,31 +39,12 @@ module.exports = {
         "SwitchCase": 1
       }
     ],
-    "jsx-a11y/aria-props": 2,
-    "jsx-a11y/heading-has-content": 0,
-    "jsx-a11y/href-no-hash": 0,
-    "jsx-a11y/anchor-is-valid": [2, { "aspects": ["invalidHref"] }],
-    "jsx-a11y/label-has-for": 2,
-    "jsx-a11y/mouse-events-have-key-events": 2,
-    "jsx-a11y/role-has-required-aria-props": 2,
-    "jsx-a11y/role-supports-aria-props": 2,
     "newline-per-chained-call": 0,
     "no-confusing-arrow": 0,
     "no-console": 1,
     "no-use-before-define": 0,
     "prefer-template": 2,
     "class-methods-use-this": 0,
-    "react/forbid-prop-types": 0,
-    "react/jsx-first-prop-new-line": [
-      2,
-      "multiline"
-    ],
-    "react/jsx-filename-extension": 0,
-    "react/jsx-no-target-blank": 0,
-    "react/require-extension": 0,
-    "react/self-closing-comp": 0,
-    "redux-saga/no-yield-in-race": 2,
-    "redux-saga/yield-effects": 2,
     "require-yield": 0,
     "import/no-webpack-loader-syntax": 0,
 
